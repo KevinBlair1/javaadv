adb2c57fb4c88c15f7b4287644ac05d3955c309e
dwijeshMishra <60146522+DwijeshMishra@users.noreply.github.com>
2020-01-25 15:40:14 +0530

    Add files via upload

---
+++ javatomysql/student.java
@@ -0,0 +1,24 @@
+
+package model;
+
+public class student {
+    private int rno;
+    private String name;
+
+    public int getRno() {
+        return rno;
+    }
+
+    public void setRno(int rno) {
+        this.rno = rno;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+}
